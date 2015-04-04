30619b2d2dc16baf27f055e70391784cc418a5ba
gladclef <benjamin.bean@comcast.net>
2015-04-04 13:07:57 -0600

    added simple box data to test with

---
+++ box_json_format.js
@@ -0,0 +1,54 @@
+// boxes are positioned at their respective id
+// box data is of the form:
+//     {alive:bool, position:{x:double,y:double}, size:{width:double, height:double}, title:null/string type_name:string, type_specific_info:{}, inputs:{}/null, outputs:{}/null}
+//     where position/size data is a scale from 0-1, where the value 1 == the drawing window width/height as appropriate
+//     where the type specific info is of the form:
+//         {properties:{}}
+//         where the properties are specified by their string name and are of the form:
+//             {onclick:string/null, display_data:{}/null}
+//             where the onclick string is the name of a corresponding function in window.box_functions
+//             where display_data is of the form:
+//                 {type:"list"/"image"/"json", values:[]/url/json}
+//     where inputs are indexed by a string key and specify a string type name
+//     where outputs are indexed by a string key and specify a string type name
+// connections are indexed by "fromboxid_toboxid" (eg "0_1")
+// connections are of the form:
+//     {from:int, output_location:string, to:int, input_location:string}
+
+// example:
+//
+// window.guidata = {boxes:[], connections:{}};
+//
+// var box = {
+//     alive: true,
+//     position: {0.4, 0.4},
+//     size: {0.2, 0.2},
+//     title: null,
+//     type_name: "type example 1",
+//     type_specific_info: {
+//         properties: {
+//             ex1: {
+//                 onclick:"do_something",
+//                 display_data: null
+//             },
+//             ex2: {
+//                 onclick:null,
+//                 display_data: {type:"list", values:[0, 1, 2]}
+//             },
+//         }
+//     },
+//     inputs: {
+//         in1: "integer",
+//         in2: "double"
+//     },
+//     outputs: {
+//         out1: "double"
+//     }
+// };
+//
+// var connection = {
+//     from: 0,
+//     output_location: "out1",
+//     to: 1,
+//     input_location: "in1"
+// };
---
+++ example_load.js
@@ -0,0 +1,92 @@
+if (window.guidata == undefined) {
+	// for an example of the data format, see box_json_format.js
+	window.guidata = {boxes:[], connections:{}};
+}
+
+create_example_boxes = function() {
+	// get the first unused box id
+	var box_id = boxes.length;
+
+	// create the first box, no connections
+	var box1 = {
+        alive: true,
+        position: {0.8, 0.8},
+        size: {0.1, 0.1},
+        title: null,
+        type_name: "type example 1",
+        type_specific_info: {
+            properties: {
+                ex1: {
+                    onclick:"do_something_1",
+                    display_data: null
+                },
+                ex2: {
+                    onclick:null,
+                    display_data: {type:"list", values:[0, 1, 2]}
+                },
+            }
+        },
+        inputs: {
+            in1: "integer",
+            in2: "double"
+        },
+        outputs: {
+            out1: "double"
+        }
+    };
+	window.guidata.boxes[box_id] = box1;
+
+	// create the second box, connects to third box
+	var box2 = {
+        alive: true,
+        position: {0.2, 0.2},
+        size: {0.1, 0.1},
+        title: null,
+        type_name: "type example 2",
+        type_specific_info: {
+            properties: {
+                ex1: {
+                    onclick:"do_something_2",
+                    display_data: null
+                }
+            }
+        },
+        inputs: null,
+        outputs: {
+            out1: "double"
+        }
+    };
+	box_id++;
+	window.guidata.boxes[box_id] = box2;
+
+	// create the second box, connects to third box
+	var box3 = {
+        alive: true,
+        position: {0.6, 0.2},
+        size: {0.1, 0.1},
+        title: null,
+        type_name: "type example 3",
+        type_specific_info: {
+            properties: {
+                ex1: {
+                    onclick:"do_something_3",
+                    display_data: null
+                }
+            }
+        },
+        inputs: {
+            in1: "double"
+        },
+        outputs: null
+    };
+	box_id++;
+	window.guidata.boxes[box_id] = box3;
+	
+	// create the connection between boxes 2 and 3
+	var connection = {
+		from: box_id - 1,
+		output_location: "out1",
+		to: box_id,
+		input_location: "in1"
+	}
+}
