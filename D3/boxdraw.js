cf64f4d4f846528d8b63a7f48d52a7c675e88efc
gladclef <benjamin.bean@comcast.net>
2015-04-06 16:14:44 -0600

    Added support for future drawing functions

--- D3/boxdraw.js
+++ D3/boxdraw.js
@@ -8,12 +8,32 @@ if (webgui.guidata == undefined) {
 }
 
 if (webgui.draw == undefined) {
-  webgui.draw = {};
+  webgui.draw = {
+    _funcs: {},
+    _add: function(name, callback) {
+      webgui.draw._funcs[name] = callback;
+      webgui.draw[name] = callback;
+    },
+    all: function() {
+      var call = function(name, callback) {
+        callback();
+      };
+      $.each(webgui.draw._funcs, call);
+    }
+  };
 }
 
-webgui.draw.boxes = function() {
+webgui.draw._add("boxes", function() {
   d3.select("#webgui_canvas").selectAll(".box").data(webgui.guidata.boxes).enter()
     .append("div")
     .classed("box", true)
     .text("hello");
-}
+});
+
+webgui.draw._add("connections", function() {
+
+});
+
+webgui.draw._add("sidebar", function() {
+
+});
