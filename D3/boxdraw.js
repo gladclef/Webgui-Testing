d83551a0c224791880c57a1bfc6c5940adfcdec8
gladclef <benjamin.bean@comcast.net>
2015-04-08 14:59:57 -0600

    added input boxes

--- D3/boxdraw.js
+++ D3/boxdraw.js
@@ -29,12 +29,34 @@ if (webgui.draw == undefined) {
   };
 }
 
+/*******************************************************************************
+ ****************************** C O N S T A N T S ******************************
+ ******************************************************************************/
+
+webgui.draw.one_tenth_width = function() { return webgui.width() / 10; };
+webgui.draw.base_font_size = function() { return webgui.draw.one_tenth_width() * 0.093; };
+webgui.draw.decorator_width = function() { return webgui.draw.one_tenth_width() * 0.077; };
+
+/*******************************************************************************
+ ************************ D R A W I N G   M E T H O D S ************************
+ ******************************************************************************/
+
 webgui.draw._add("boxes", function() {
   "use strict";
 
   // dimensions to be used to create the box
   var w = webgui.box_canvas_width();
   var h = webgui.box_canvas_height();
+  var get_set_width = function(d) {
+    var width = w * d.size.width;
+    d.size.pixel_width = width;
+    return (width + "px");
+  };
+  var get_set_height = function(d) {
+    var height = h * d.size.height;
+    d.size.pixel_height = height;
+    return (height + "px");
+  };
 
   // create the box
   var box = webgui.box_canvas().selectAll(".box").data(webgui.guidata.boxes).enter()
@@ -43,8 +65,8 @@ webgui.draw._add("boxes", function() {
     .attr("box_id", function(d, i) { return i; })
     .style("top", function(d) { return h * d.position.y + "px" })
     .style("left", function(d) { return w * d.position.x + "px"})
-    .style("width", function(d) { return w * d.size.width + "px"})
-    .style("height", function(d) { return h * d.size.height + "px"});
+    .style("width", get_set_width)
+    .style("height", get_set_height);
 
   // add internals to the box
   webgui.draw.box_internals();
@@ -70,35 +92,96 @@ webgui.draw._add("box_internals", function() {
       prop.title = k;
       retval[retval.length] = prop;
     };
-    $.each(d.type_specific_info.properties, add_to_retval);
-    console.log(retval);
+    if (d.type_specific_info.properties != null) {
+      $.each(d.type_specific_info.properties, add_to_retval);
+    }
     return retval;
   };
   var get_prop_title = function(d) {
     return d.title;
   };
+  var font_size = webgui.draw.base_font_size();
 
+  // get the boxes to be updated
   var needs_update = function(d) {
     var do_update = d.needs_update.internals;
     return do_update;
   };
   var update_boxes = webgui.guidata.live_boxes().filter(needs_update);
-
   var boxes = webgui.box_canvas().selectAll(".box").data(update_boxes);
+
+  // update the title
   boxes.selectAll(".title").data(get_title_data).exit().remove();
   boxes.selectAll(".title").data(get_title_data).enter()
     .append("div")
-    .classed({title:true})
+    .classed({title:true, internals:true})
+    .style({
+      height: (font_size * 1.5 + 6 + "px"),
+      "font-size": (font_size * 1.5 + "px")
+    })
     .text(get_title);
+
+  // update the properties
   boxes.selectAll(".property").data(get_props_data).exit().remove();
   boxes.selectAll(".property").data(get_props_data).enter()
     .append("div")
-    .classed({property:true})
+    .classed({property:true, internals:true})
+    .style({
+      height: (font_size + 6 + "px"),
+      "font-size": (font_size + "px")
+    })
     .text(get_prop_title);
 });
 
 webgui.draw._add("box_decorations", function() {
+  var get_decorators = function(decorators, size) {
+    var retval = [];
+    var add_to_retval = function(k,v) {
+      var decorator = {type:v};
+      decorator.title = k;
+      decorator.count = Object.keys(decorators).length;
+      decorator.box_size = size;
+      retval[retval.length] = decorator;
+    };
+    if (decorators != null) {
+      $.each(decorators, add_to_retval);
+    }
+    return retval;
+  };
+  var get_inputs = function(d) {
+    return get_decorators(d.inputs, d.size);
+  };
+  var get_outputs = function(d) {
+    return get_decorators(d.outputs, d.size);
+  };
+  var get_left = function(d, i) {
+    var offset = d.box_size.pixel_width / (d.count + 1) * (i + 1) - decorator_width / 2;
+    return (offset + "px");
+  };
+  var get_top = function(d) {
+    var top = d.box_size.pixel_height + decorator_width / 2;
+    return (top + "px");
+  };
+  var decorator_width = webgui.draw.decorator_width();
 
+  // get the boxes to be updated
+  var needs_update = function(d) {
+    var do_update = d.needs_update.decorators;
+    return do_update;
+  };
+  var update_boxes = webgui.guidata.live_boxes().filter(needs_update);
+  var boxes = webgui.box_canvas().selectAll(".box").data(update_boxes);
+
+  boxes.selectAll(".input").data(get_inputs).exit().remove();
+  boxes.selectAll(".input").data(get_inputs).enter()
+    .append("div")
+    .classed({input:true, decorators:true})
+    .style({
+      width: (decorator_width + "px"),
+      height: (decorator_width + "px"),
+      top: (-decorator_width/2 + "px")
+    })
+    .style("left", get_left);
 });
 
 webgui.draw._add("connections", function() {
